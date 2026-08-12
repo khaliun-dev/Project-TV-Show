caae9106c0e9ff09a0fdb3d77eaba18bc6aaed67
JorvanW <your.email@example.com>
2026-08-12 14:17:19 +0100

    Updated necessary files for level 100

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -7,6 +6,47 @@ function setup() {
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
   rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+}
+
+const allEps = getAllEpisodes();
+
+// for (const element of allEps) {
+//   // ...use `element`...
+//   console.log(allEps);
+// }
+
+function makePageForEpisodes(episodeList) {
+  const rootElem = document.getElementById("root");
+
+  episodeList.forEach(function (episode) {
+    const episodeBox = document.createElement("div");
+
+    episodeBox.className = "episode";
+
+    const episodeCode = `S${String(episode.season).padStart(2, "0")}E${String(episode.number).padStart(2, "0")}`;
+
+    episodeBox.innerHTML = `
+      <h2>${episode.name}
+      
+  <p>${episodeCode}</p></h2>
+
+      <img src="${episode.image.medium}" alt="${episode.name}">
+
+      <p>Season ${episode.season}, Episode ${episode.number}</p>
+
+      <p>${episode.summary}</p>
+
+      <p>Air date: ${episode.airdate}</p>
+
+      <p>Runtime: ${episode.runtime} minutes</p>
+
+      <a href="${episode.url}" target="_blank">
+        View episode
+      </a>
+    `;
+
+    rootElem.appendChild(episodeBox);
+  });
 }
 
 window.onload = setup;
