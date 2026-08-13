c4496414e8809e8a06a35ce1184f496a1aa2ab90
Khaliun <khaliun13@hotmail.com>
2026-08-14 00:16:24 +0100

    feat: display all TV show episodes

--- script.js
+++ script.js
@@ -1,4 +1,3 @@
-//You can edit ALL of the code here
 function setup() {
   const allEpisodes = getAllEpisodes();
   makePageForEpisodes(allEpisodes);
@@ -6,7 +5,29 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+
+  for (const episode of episodeList) {
+    const title = document.createElement("h2");
+    title.textContent = episode.name;
+
+    const season = String(episode.season).padStart(2, "0");
+    const number = String(episode.number).padStart(2, "0");
+
+    const episodeCode = document.createElement("p");
+    episodeCode.textContent = `S${season}E${number}`;
+
+    const image = document.createElement("img");
+    image.src = episode.image.medium;
+    image.alt = episode.name;
+
+    const summary = document.createElement("p");
+    summary.innerHTML = episode.summary;
+
+    rootElem.appendChild(title);
+    rootElem.appendChild(episodeCode);
+    rootElem.appendChild(image);
+    rootElem.appendChild(summary);
+  }
 }
 
 window.onload = setup;
